ae9271599fb82119d614d34bf0fb33efc7a53f83
TheBawMan <104532960+TheBawMan@users.noreply.github.com>
2024-07-04 01:34:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,39 +11,36 @@ import { Toaster } from "react-hot-toast";
 import { useEffect, useState } from "react";
 
 function App() {
-  // const [menuOpen, setMenuOpen] = useState(false);
-  // const [ratio, setRatio] = useState(window.innerWidth / window.innerHeight);
-  // useEffect(() => {
-  //   const resizeRatio = () => {
-  //     setRatio(window.innerWidth / window.innerHeight);
-  //   };
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [ratio, setRatio] = useState(window.innerWidth / window.innerHeight);
+  useEffect(() => {
+    const resizeRatio = () => {
+      setRatio(window.innerWidth / window.innerHeight);
+    };
 
-  //   window.addEventListener("resize", resizeRatio);
+    window.addEventListener("resize", resizeRatio);
 
-  //   return () => {
-  //     window.removeEventListener("resize", resizeRatio);
-  //   };
-  // }, [ratio]);
+    return () => {
+      window.removeEventListener("resize", resizeRatio);
+    };
+  }, [ratio]);
 
-  // return ratio < 2.2 ? (
-  //   <>
-  //     <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-  //     <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-  //     <Home ratio={ratio} />
-  //     <Work />
-  //     <Timeline />
-  //     <Services />
-  //     <Testimonial />
-  //     <Contact />
-  //     <Footer />
-  //     <Toaster />
-  //   </>
-  // ) : (
-  //   <em id="customMessage">Please Change the ratio to View!</em>
-  // );
-  return () =>{
-    <MaintenanceBanner/>
-    }
-}
+  return ratio < 5.2 ? (
+    <>
+      <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home ratio={ratio} />
+      <Work />
+      <Timeline />
+      <Services />
+      <Testimonial />
+      <Contact />
+      <Footer />
+      <Toaster />
+    </>
+  ) : (
+    <em id="customMessage">This Site is Under Maintenance! New & Rwdesigned Version is Coming...</em>
+  );
+ 
 
 export default App;
